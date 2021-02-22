2e0a5ddf538bbb9a22e99ac24b23abc9994776bd
Priyank Dev Varshney <priyankdevvarshney@Priyanks-MacBook-Pro.local>
2021-02-22 11:00:17 +0530

    boiler

---
+++ src/main/java/com/example/SprinProject/SprinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SprinProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinProjectApplication.class, args);
+	}
+
+}
